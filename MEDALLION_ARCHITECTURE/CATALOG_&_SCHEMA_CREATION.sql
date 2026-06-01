204bebfe07838a0e682907c6c6322d6fe6479296
Mohammad Faizan <mohammadfaizan0411@gmail.com>
2026-06-01 19:01:37 +0530

    Update and rename CATALOG_&_SCHEMA_CREATION.py to CATALOG_&_SCHEMA_CREATION.sql

---
+++ MEDALLION_ARCHITECTURE/CATALOG_&_SCHEMA_CREATION.sql
@@ -0,0 +1,28 @@
+-- Databricks notebook source
+
+CREATE CATALOG IF NOT EXISTS `ecom-medallion-pipeline`;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS `ecom-medallion-pipeline`.raw;
+CREATE SCHEMA IF NOT EXISTS `ecom-medallion-pipeline`.bronze;
+CREATE SCHEMA IF NOT EXISTS `ecom-medallion-pipeline`.silver;
+CREATE SCHEMA IF NOT EXISTS `ecom-medallion-pipeline`.gold;
+
+-- COMMAND ----------
+
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.raw.mobile_orders;
+
+-- COMMAND ----------
+
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.bronze.mobile_orders;
+
+-- COMMAND ----------
+
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.silver.mobile_orders;
+
+-- COMMAND ----------
+
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.gold.price_segments;
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.gold.brand_summary;
+CREATE VOLUME IF NOT EXISTS `ecom-medallion-pipeline`.gold.top_rated_phones;
